d24731e8258f845acd1a33d2df39155b0e7efe62
Asserewou Etekpo <aetekpo@gmail.com>
2025-04-20 22:48:32 -0500

    Fix import path for AwsCdkGithubStack

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import aws_cdk as cdk
-from cdk_github_stack import AwsCdkGithubStack
+from aws_cdk_github.aws_cdk_github_stack import AwsCdkGithubStack
 
 app = cdk.App()
 AwsCdkGithubStack(app, "AwsCdkGithubStack")
