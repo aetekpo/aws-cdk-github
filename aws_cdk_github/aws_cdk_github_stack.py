d2969d94ebb94072eb639e236c8c6f0ee2a23e19
Asserewou Etekpo <aetekpo@gmail.com>
2025-04-20 22:26:00 -0500

    Updated CDK stack and added Lambda code

--- aws_cdk_github/aws_cdk_github_stack.py
+++ aws_cdk_github/aws_cdk_github_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    Duration
 )
 from constructs import Construct
 
@@ -10,10 +11,17 @@ class AwsCdkGithubStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Create an S3 bucket
+        bucket = s3.Bucket(self, "MyFirstBucket",
+            versioned=True,
+            bucket_name="cdk-github-demo-bucket",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkGithubQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create a Lambda function
+        fn = _lambda.Function(self, "MyLambdaFunction",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler="demo_lambda.lambda_handler",
+            code=_lambda.Code.from_asset("lambda_code_demo"),
+            timeout=Duration.seconds(30)
+        )
